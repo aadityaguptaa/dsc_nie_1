9acdb807994a22ac4318b95796d45266944e7e43
aadityaguptaa <rockonman3@gmail.com>
2021-04-08 15:01:13 +0530

    techTeam t-1

--- app/src/main/java/com/example/dsc_nie/RecyclerAdapter.kt
+++ app/src/main/java/com/example/dsc_nie/RecyclerAdapter.kt
@@ -1,28 +1,45 @@
 package com.example.dsc_nie
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.dsc_nie.R
 
-class RecyclerAdapter (private var titles: List<String>, private var details: List<String>, private var images:List<Int>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+class RecyclerAdapter (private var titles: List<String>, private var details: List<String>, private var images:List<Int>, private var description: List<String>, private var instagramLink: List<String>, private var facebookLink: List<String>, private var emailLink: List<String>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
 
-        val itemTitle: TextView = itemView.findViewById(R.id.item_title)
-        val itemDetails: TextView = itemView.findViewById(R.id.item_description)
-        val itemPicture: ImageView = itemView.findViewById(R.id.item_imageView)
+        val itemTitle: TextView = itemView.findViewById(R.id.CoreName)
+        val itemDetails: TextView = itemView.findViewById(R.id.CorePosition)
+        val itemPicture: ImageView = itemView.findViewById(R.id.CoreImage)
+        val itemDescription: TextView = itemView.findViewById(R.id.CoreDescription)
+        val cardView: CardView = itemView.findViewById(R.id.cardView)
+        val expandableLayout: LinearLayout = itemView.findViewById(R.id.expandableLayout)
+        val coreInstagram: TextView = itemView.findViewById(R.id.CoreInstagramLink)
+        val coreFacebook: TextView = itemView.findViewById(R.id.CoreFacebookLink)
+        val coreEmail: TextView = itemView.findViewById(R.id.CoreEmailLink)
+
 
 
         init{
-            itemView.setOnClickListener { v: View ->
+
+            itemView.setOnClickListener {
+                if (expandableLayout.visibility == View.GONE) {
+                    TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                    expandableLayout.visibility = View.VISIBLE
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                    expandableLayout.visibility = View.GONE
+                }
+            }
+            /*itemView.setOnClickListener { v: View ->
                 val position: Int = adapterPosition
                 Toast.makeText(itemView.context, "You clicked on item # ${position + 1}", Toast.LENGTH_SHORT).show()
-            }
+            }*/
         }
     }
 
@@ -35,7 +52,10 @@ class RecyclerAdapter (private var titles: List<String>, private var details: Li
         holder.itemTitle.text = titles[position]
         holder.itemDetails.text = details[position]
         holder.itemPicture.setImageResource(images[position])
-
+        holder.itemDescription.text = description[position]
+        holder.coreInstagram.text = instagramLink[position]
+        holder.coreFacebook.text = facebookLink[position]
+        holder.coreEmail.text = emailLink[position]
 
     }
 
--- app/src/main/java/com/example/dsc_nie/teams_intro/TechTeamFragment.kt
+++ app/src/main/java/com/example/dsc_nie/teams_intro/TechTeamFragment.kt
@@ -15,8 +15,14 @@ import com.example.dsc_nie.databinding.FragmentTechTeamBinding
 
 class TechTeamFragment : Fragment() {
     private var titlesList = mutableListOf<String>()
-    private var descList = mutableListOf<String>()
+    private var positionList = mutableListOf<String>()
     private var imagesList = mutableListOf<Int>()
+    private var descList = mutableListOf<String>()
+    private var instagramList = mutableListOf<String>()
+    private var facebookList = mutableListOf<String>()
+    private var emailList = mutableListOf<String>()
+
+
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +36,7 @@ class TechTeamFragment : Fragment() {
 
         recyclerView =  binding.techTeamRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = RecyclerAdapter(titlesList, descList, imagesList)
+        recyclerView.adapter = RecyclerAdapter(titlesList, positionList, imagesList, descList, instagramList, facebookList, emailList)
 
         return binding.root
     }
@@ -40,21 +46,25 @@ class TechTeamFragment : Fragment() {
 
     }
 
-    private fun addToList(title: String, description: String, image: Int){
+    private fun addToList(title: String, position: String, image: Int, description: String, instagram: String, facebook: String, email: String){
         titlesList.add(title)
-        descList.add(description)
+        positionList.add(position)
         imagesList.add(image)
+        descList.add(description)
+        instagramList.add(instagram)
+        facebookList.add(facebook)
+        emailList.add(email)
     }
 
     private fun postToList(){
-        addToList(getString(R.string.dsc_lead), getString(R.string.pratyusha), R.drawable.pratyusha)
-        addToList(getString(R.string.android_lead), getString(R.string.aditya), R.drawable.adityaone)
-        addToList(getString(R.string.ml_lead), getString(R.string.harsh), R.drawable.harsh)
-        addToList(getString(R.string.web_dev_lead_lead), getString(R.string.iresh), R.drawable.iresh)
-        addToList(getString(R.string.technical_advisor), getString(R.string.pranav), R.drawable.pranav)
-        addToList(getString(R.string.web_dev_lead_lead), getString(R.string.rishabh), R.drawable.rishabh)
-        addToList(getString(R.string.technical_lead), getString(R.string.sanskar), R.drawable.sanskar)
-        addToList(getString(R.string.microcontrollers_lead), getString(R.string.shujan), R.drawable.shujan)
-        addToList(getString(R.string.android_lead), getString(R.string.tanmoy), R.drawable.tanmoy)
+        addToList(getString(R.string.pratyusha), getString(R.string.dsc_lead), R.drawable.pratyushaone, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.aditya),getString(R.string.android_lead),  R.drawable.adityaone, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.harsh),getString(R.string.ml_lead),  R.drawable.harsh, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.iresh),getString(R.string.web_dev_lead_lead),  R.drawable.iresh, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.pranav),getString(R.string.technical_advisor),  R.drawable.pranav, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.rishabh),getString(R.string.web_dev_lead_lead),  R.drawable.rishabh, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.sanskar),getString(R.string.technical_lead),  R.drawable.sanskar, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.shujan),getString(R.string.microcontrollers_lead),  R.drawable.shujan, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
+        addToList(getString(R.string.tanmoy),getString(R.string.android_lead),  R.drawable.tanmoy, getString(R.string.adityaDescription), getString(R.string.aditya_instagram), getString(R.string.aditya_facebook), getString(R.string.aditya_email))
     }
 }
