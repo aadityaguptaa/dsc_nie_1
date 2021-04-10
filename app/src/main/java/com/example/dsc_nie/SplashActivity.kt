48b0687de8be89b574ad781d8ddc45caf1fb01af
aadityaguptaa <rockonman3@gmail.com>
2021-04-10 15:19:19 +0530

    splashScreen Done

---
+++ app/src/main/java/com/example/dsc_nie/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.dsc_nie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.databinding.DataBindingUtil
+import com.example.dsc_nie.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //inflating the splashScreen Activity
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        //Transition in the Splash Screen
+        binding.motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
+        })
+    }
+}
