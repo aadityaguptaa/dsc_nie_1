5546709ca0020e915746904abffaf77497a4fe5e
aadityaguptaa <rockonman3@gmail.com>
2021-04-14 18:00:08 +0530

    Event linked to firebase t-1

--- app/src/main/java/com/example/dsc_nie/events/EventItemFragment.kt
+++ app/src/main/java/com/example/dsc_nie/events/EventItemFragment.kt
@@ -1,19 +1,56 @@
 package com.example.dsc_nie.events
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.dsc_nie.R
+import com.example.dsc_nie.databinding.EventItemBottomSheetBinding
+import com.example.dsc_nie.databinding.FragmentEventItemBinding
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.Exception
 
 
 class EventItemFragment : Fragment() {
-
+    lateinit var binding: FragmentEventItemBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_item, container, false)
+
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_event_item, container, false
+        )
+
+        try {
+            var firebaseDatabase = FirebaseDatabase.getInstance().reference.child("aaaaaa").get().addOnCompleteListener {it ->
+                Log.i("abc", it.result!!.child("Title").getValue() as String)
+
+                val Date = it.result!!.child("Date").getValue() as String
+                val Details = it.result!!.child("Details").getValue() as String
+                val Location = it.result!!.child("Location").getValue() as String
+                binding.EventFragmentProgressBar.visibility = View.GONE
+                binding.EventItemCoordinatorLayout.visibility = View.VISIBLE
+                container!!.findViewById<TextView>(R.id.EventLocationTextView).text = Location
+                container!!.findViewById<TextView>(R.id.EventDateTextView).text = Date
+                container!!.findViewById<TextView>(R.id.EventDetailsTextView).text = Details
+            }
+
+        }catch (e: Exception){
+            Toast.makeText(
+                context,
+                "Something went wrong, please try again/n Exception: "+ e.cause,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+        return binding.root
     }
+
 
 }
